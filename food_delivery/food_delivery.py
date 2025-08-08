de0a614bfb7f98f2fd5f9cd549761bc5ceed8281
Hana Kessler <hanakessler18@gmail.com>
2025-08-08 09:42:55 -0400

    food delivery

---
+++ food_delivery/food_delivery.py
@@ -0,0 +1,15 @@
+"""
+The problem description for this problem is in prompts/food_delivery.md.
+
+To use, read through the problem description linked above - starting just with level 1.
+Not looking ahead most accurately simulates how things like CodeSignal progressively show you the problem.
+
+After implementing the first level, run `python food_delivery/test.py --level 1` and progress through all levels to level 4.
+
+See the main readme for disclaimers and other info.
+"""
+
+
+class FoodDeliverySystem:
+    # TODO implementme
+    pass
